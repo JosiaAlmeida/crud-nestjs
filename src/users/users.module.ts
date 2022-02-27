0d093211c77776958929f481fbd881d925d89e85
JosiaAlmeida <josiaalmeida0001@gmail.com>
2022-02-27 19:50:15 +0100

    fix: erro no enderecamento do schema

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserSchema } from './schemas/user.schema';
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
 
-@Module({})
+@Module({
+  imports: [MongooseModule.forFeature([{ name: 'User', schema: UserSchema }])],
+  controllers: [UsersController],
+  providers: [UsersService],
+})
 export class UsersModule {}
